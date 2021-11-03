2e285e704b9f625e867fecc50b5b209069cc282b
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-11-03 09:59:08 -0500

    spring views intro

---
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -0,0 +1,44 @@
+package com.codeup.springblog.controllers;
+
+import com.codeup.springblog.models.Coffee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CoffeeController {
+
+    @GetMapping("/coffee")
+    public String coffeeInfo() {
+        return "views-lec/coffee";
+    }
+
+    @GetMapping("/coffee/{roast}")
+    public String roastSelection(@PathVariable String roast, Model model) {
+
+        Coffee selection = new Coffee(roast, "Cool Beans");
+        Coffee selection2 = new Coffee(roast, "Jolting Joe");
+        if (roast.equals("dark")) {
+            selection.setOrigin("Columbia");
+            selection2.setOrigin("Brazil");
+        } else if (roast.equals("medium")) {
+            selection.setOrigin("New Guinea");
+            selection2.setOrigin("Sumatra");
+        } else {
+            selection.setOrigin("Kenya");
+            selection2.setOrigin("Ethiopia");
+        }
+        List<Coffee> selections = new ArrayList<>();
+        selections.add(selection);
+        selections.add(selection2);
+        model.addAttribute("roast", roast);
+        model.addAttribute("selection", selection);
+        return "views-lec/coffee";
+    }
+
+
+}
