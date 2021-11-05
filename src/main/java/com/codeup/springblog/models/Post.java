1dd1896db871b20a341158dd59d9ca58b1afd7bc
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-11-05 11:16:18 -0500

    added to views completed exercise

---
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -0,0 +1,47 @@
+package com.codeup.springblog.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String body;
+
+    public Post(){
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+
+}
