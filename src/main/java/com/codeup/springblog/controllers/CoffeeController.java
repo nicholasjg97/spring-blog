64b2d4bec3eb4521e042193a6ed91409976dc09f
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-11-08 14:23:54 -0600

    form binding lec

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -1,10 +1,10 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Coffee;
+import com.codeup.springblog.repositories.CoffeeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,33 +12,34 @@ import java.util.List;
 @Controller
 public class CoffeeController {
 
+    private final CoffeeRepository coffeeRepository;
+
+    public CoffeeController(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
+    }
+
     @GetMapping("/coffee")
     public String coffeeInfo() {
-        return "views-lec/coffee";
+        return "/coffees/coffee";
     }
 
     @GetMapping("/coffee/{roast}")
     public String roastSelection(@PathVariable String roast, Model model) {
 
-        Coffee selection = new Coffee(roast, "Cool Beans");
-        Coffee selection2 = new Coffee(roast, "Jolting Joe");
-        if (roast.equals("dark")) {
-            selection.setOrigin("Columbia");
-            selection2.setOrigin("Brazil");
-        } else if (roast.equals("medium")) {
-            selection.setOrigin("New Guinea");
-            selection2.setOrigin("Sumatra");
-        } else {
-            selection.setOrigin("Kenya");
-            selection2.setOrigin("Ethiopia");
-        }
-        List<Coffee> selections = new ArrayList<>();
-        selections.add(selection);
-        selections.add(selection2);
-        model.addAttribute("roast", roast);
-        model.addAttribute("selection", selection);
-        return "views-lec/coffee";
+        model.addAttribute("selections", coffeeRepository.findByRoast(roast));
+
+        return "/coffees/coffee";
     }
 
+    @GetMapping("/coffee/create")
+    public String showCreateCoffee(Model model){
+        model.addAttribute("coffee", new Coffee());
+        return "coffees/create";
+    }
 
+    @PostMapping("/coffee/create")
+    public String createCoffee(@ModelAttribute Coffee coffee) {
+        coffeeRepository.save(coffee);
+        return "redirect:/coffee";
+    }
 }
